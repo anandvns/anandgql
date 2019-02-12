e70f6c83542983f5b9c52fe0218b636c3a9ffd4a
anandvns <anand.sunny.7@gmail.com>
2019-02-12 23:29:10 +0530

    Added Users Service

---
+++ app/global/config/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+
+    server: {
+        PORT: process.env.PORT || 1221,
+    },
+
+    database: {
+        HOST: process.env.MONGODB || 'mongodb://admin:admin$101@ds037165.mlab.com:37165/graphql',
+    },
+
+};
